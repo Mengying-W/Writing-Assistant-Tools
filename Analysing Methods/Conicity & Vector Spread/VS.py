4f3d8e5dc26c07163185d38e62915ec90b2ec6ae
mengying wang <mengying.w.wang@gmail.com>
2024-10-11 15:56:23 +0200

    Update VS.py
    clean code

--- Analysing Methods/Conicity & Vector Spread/VS.py
+++ Analysing Methods/Conicity & Vector Spread/VS.py
@@ -1,4 +1,3 @@
-# %%
 from scipy.spatial import ConvexHull
 from sklearn.decomposition import PCA
 import pandas as pd
@@ -6,7 +5,6 @@ from tqdm import tqdm
 import os
 import numpy as np
 
-# %%
 def getBERTcasevecs(veclist):
 
     sensvecslist = veclist.split('],') #all sen vec list for every para
@@ -91,25 +89,23 @@ def getsenBERTvecs(veclist):
     lenth = len(float_vectors)
     return float_vectors,lenth
 
-# %%
-df1 = pd.read_csv('/home/pop532211/WATs/processed/word/emvcOfgpt2-1.csv')
+df1 = pd.read_csv('../emvcOfgpt2-1.csv')
 df1 = df1.drop(df1.columns[0],axis=1)
-df2 = pd.read_csv('/home/pop532211/WATs/processed/word/emvcOfgpt2-2.csv')
+df2 = pd.read_csv('../emvcOfgpt2-2.csv')
 df2 = df2.drop(df2.columns[0],axis=1)
-df3 = pd.read_csv('/home/pop532211/WATs/processed/word/emvcOfgpt2-3.csv')
+df3 = pd.read_csv('../emvcOfgpt2-3.csv')
 df3 = df3.drop(df3.columns[0],axis=1)
-df4 = pd.read_csv('/home/pop532211/WATs/processed/word/emvcOfgpt2-4.csv')
+df4 = pd.read_csv('../emvcOfgpt2-4.csv')
 df4 = df4.drop(df4.columns[0],axis=1)
-df5 = pd.read_csv('/home/pop532211/WATs/processed/word/emvcOfgpt2-5.csv')
+df5 = pd.read_csv('../emvcOfgpt2-5.csv')
 df5 = df5.drop(df5.columns[0],axis=1)
-df6 = pd.read_csv('/home/pop532211/WATs/processed/word/emvcOfgpt2-6.csv')
+df6 = pd.read_csv('../emvcOfgpt2-6.csv')
 df6 = df6.drop(df6.columns[0],axis=1)
 
 df = pd.concat([df1,df2,df3,df4,df5,df6], ignore_index=True, axis=1)
 heads = ['Original','Rephrase','Grammarly','ChatGPT','Wordtune','Quillbot','Vicuna','GPT_4','Flan_T5_1','Flan_T5_2','Flan_T5_3','Flan_T5_4','Flan_T5_5','Flan_T5_6','Flan_T5_7','Flan_T5_8','Flan_T5_9','Flan_T5_10']
 df.columns = heads
 
-# %%
 df_allvec = pd.DataFrame()
 
 for head in heads:
@@ -124,25 +120,23 @@ for head in heads:
         float_vecs.append(vectors)
     df_allvec[head] = float_vecs
 
-# %%
-df1 = pd.read_csv('/home/pop532211/WATs/meanvecs/word/meanvecs_gpt2-1.csv')
+df1 = pd.read_csv('../meanvecs_gpt2-1.csv')
 df1 = df1.drop(df1.columns[0],axis=1)
-df2 = pd.read_csv('/home/pop532211/WATs/meanvecs/word/meanvecs_gpt2-2.csv')
+df2 = pd.read_csv('../meanvecs_gpt2-2.csv')
 df2 = df2.drop(df2.columns[0],axis=1)
-df3 = pd.read_csv('/home/pop532211/WATs/meanvecs/word/meanvecs_gpt2-3.csv')
+df3 = pd.read_csv('../meanvecs_gpt2-3.csv')
 df3 = df3.drop(df3.columns[0],axis=1)
-df4 = pd.read_csv('/home/pop532211/WATs/meanvecs/word/meanvecs_gpt2-4.csv')
+df4 = pd.read_csv('../meanvecs_gpt2-4.csv')
 df4 = df4.drop(df4.columns[0],axis=1)
-df5 = pd.read_csv('/home/pop532211/WATs/meanvecs/word/meanvecs_gpt2-5.csv')
+df5 = pd.read_csv('../meanvecs_gpt2-5.csv')
 df5 = df5.drop(df5.columns[0],axis=1)
-df6 = pd.read_csv('/home/pop532211/WATs/meanvecs/word/meanvecs_gpt2-6.csv')
+df6 = pd.read_csv('../meanvecs_gpt2-6.csv')
 df6 = df6.drop(df6.columns[0],axis=1)
 
 df_mean = pd.concat([df1,df2,df3,df4,df5,df6], ignore_index=True, axis=1)
 heads = ['Original','Rephrase','Grammarly','ChatGPT','Wordtune','Quillbot','Vicuna','GPT_4','Flan_T5_1','Flan_T5_2','Flan_T5_3','Flan_T5_4','Flan_T5_5','Flan_T5_6','Flan_T5_7','Flan_T5_8','Flan_T5_9','Flan_T5_10']
 df_mean.columns = heads
 
-# %%
 df_mean_vec = pd.DataFrame()
 for head in heads:
     digit_value = []
@@ -158,11 +152,9 @@ for head in heads:
         digit_value.append(value)
     df_mean_vec[head] = digit_value
 
-# %%
-df_conicity = pd.read_csv('/home/pop532211/WATs/conicity/word/conicity_gpt2.csv')
+df_conicity = pd.read_csv('../conicity_gpt2.csv')
 df_conicity = df_conicity.drop(df_conicity.columns[0],axis=1)
 
-# %%
 df_VS = pd.DataFrame()
 
 for head in heads:
@@ -190,10 +182,8 @@ for head in heads:
         
     df_VS[head] = all_vector_spread
 
-# %%
-df_VS.to_csv('/home/pop532211/WATs/vector spread/vector_spread_gpt2.csv')
+df_VS.to_csv('../vector_spread_gpt2.csv')
 
-# %%
 def getBERTcasevecs(veclist):
 
     sensvecslist = veclist.split('],') #all sen vec list for every para
@@ -277,8 +267,3 @@ def getsenBERTvecs(veclist):
         float_vectors.append(item)
     lenth = len(float_vectors)
     return float_vectors,lenth
-
-# %%
-
-
-
