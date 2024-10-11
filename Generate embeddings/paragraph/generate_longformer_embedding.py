73b52f5b0f22be15ca371a990f28fa829fc1fb52
mengying wang <mengying.w.wang@gmail.com>
2024-10-11 15:42:30 +0200

    Update generate_longformer_embedding.py
    clean code

--- Generate embeddings/paragraph/generate_longformer_embedding.py
+++ Generate embeddings/paragraph/generate_longformer_embedding.py
@@ -1,26 +1,15 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-
 import torch
 from transformers import LongformerTokenizer, LongformerModel
 import pandas as pd
 from tqdm import tqdm
 import numpy as np
 
-
-# In[2]:
-
-
 # Load the Longformer tokenizer and model
 model = LongformerModel.from_pretrained("allenai/longformer-large-4096")
 tokenizer = LongformerTokenizer.from_pretrained("allenai/longformer-large-4096")
-
-
-# In[7]:
-
 
 # Define a function to compute the document embedding vector
 def get_document_embedding(document):
@@ -36,15 +25,7 @@ def get_document_embedding(document):
     doc_embedding = torch.mean(last_hidden_states, dim=1).squeeze().tolist()
     return doc_embedding
 
-
-# In[8]:
-
-
-df = pd.read_csv('/home/pop532211/WATs/generate_embeddings/annotated data.csv')
-
-
-# In[9]:
-
+df = pd.read_csv('../annotated data.csv')
 
 df_vec = pd.DataFrame()
 for index in df:
@@ -56,27 +37,4 @@ for index in df:
         final_vec.append(embedding)
     df_vec[index] = final_vec
 
-
-# In[10]:
-
-
-df_vec.to_csv('/home/pop532211/WATs/processed/paragraph/doc2vec_longformer.csv')
-
-
-# In[11]:
-
-
-a = df_vec["Grammarly"]
-
-
-# In[14]:
-
-
-len(a[166])
-
-
-# In[ ]:
-
-
-
-
+df_vec.to_csv('../doc2vec_longformer.csv')
